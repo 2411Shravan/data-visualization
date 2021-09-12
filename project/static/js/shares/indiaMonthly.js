2ad54ecf3bed5c62abb474894ee4e3e9578962dc
Shravan .V <perfectparameter24@gmail.com>
2021-09-12 10:55:49 +0530

    indian shares weekly data

--- project/static/js/shares/indiaMonthly.js
+++ project/static/js/shares/indiaMonthly.js
@@ -12,6 +12,10 @@ IndiantickersMonthly.addEventListener('submit', (e) => {
     getIndianMonthlyData(updata);
 });
 
+var keyMonth=[];
+var valueMonth=[];
+var final_resultMonth=[];
+
 function getIndianMonthlyData(getData){
     console.log(getData);
     var api='https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='+getData+'.BSE&apikey=WH75LQJ4BD7S15TO'
@@ -22,4 +26,35 @@ async function fetchIndianMonthlyData(API){
     const response = await fetch(API);
     const responseData=await response.json();
     console.log(responseData);
+    sortmonthlydata(responseData);
+}
+
+function sortmonthlydata(rd){
+    console.log(rd);
+    var redData = rd['Monthly Time Series'];
+    var ref_val = Object.values(redData);
+    var ref_key = Object.keys(redData);
+    console.log(ref_val);
+    console.log(ref_key);
+    updateIndianMonthlyData(ref_key, ref_val);
+}
+
+function updateIndianMonthlyData(keys,values){
+    keys.forEach(data => {
+        keyMonth.push(data);
+    });
+    // console.log(key);
+    values.forEach(data => {
+        var temp_arr = [];
+        temp_arr.push(parseFloat(data['1. open']));
+        temp_arr.push(parseFloat(data['2. high']));
+        temp_arr.push(parseFloat(data['3. low']));
+        temp_arr.push(parseFloat(data['4. close']));
+        valueMonth.push(temp_arr);
+    });
+    mergeMonthlyData();
+}
+
+function mergeMonthlyData(){
+
 }
