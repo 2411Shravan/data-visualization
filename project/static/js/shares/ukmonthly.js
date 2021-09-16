3b79e07d262f56c6956ee4648f87276026640646
Shravan .V <perfectparameter24@gmail.com>
2021-09-16 10:47:22 +0530

    working on united kingdom shares page

--- project/static/js/shares/ukmonthly.js
+++ project/static/js/shares/ukmonthly.js
@@ -1,19 +1,19 @@
 const UKtickersMonthly = document.getElementById('UKtickersMonthly');
 // const loaderDouble = document.getElementById('loaderDouble');
-var monthly_dates = [];
-var monthly_ohlc = [];
-var final_monthly = [];
+var uk_monthly_dates = [];
+var uk_monthly_ohlc = [];
+var uk_final_monthly = [];
 
 UKtickersMonthly.addEventListener('submit', (e) => {
 
     e.preventDefault();
     // loaderDouble.style.display = 'block';
-    const req_data = document.getElementById('tickerM').value;
+    const req_data = document.getElementById('UKtickerM').value;
     console.log(req_data);
-    ready_monthly_data(req_data);
+    UKready_monthly_data(req_data);
 });
 
-function ready_monthly_data(rd){
+function UKready_monthly_data(rd){
     var stre=rd.toUpperCase();
     console.log(stre);
     var api='https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol='+stre+'.LON&apikey=WH75LQJ4BD7S15TO';
@@ -24,25 +24,25 @@ async function fetchMonthlyData(API){
     const response = await fetch(API);
     const responseData = await response.json();
     // console.log(responseData);
-    sortMontlyData(responseData);
+    sortMontlyUKData(responseData);
 }
 
-function sortMontlyData(responses){
+function sortMontlyUKData(responses){
     var redData = responses['Monthly Adjusted Time Series'];
     var ref_val = Object.values(redData);
     var ref_key = Object.keys(redData);
     // console.log(ref_key,ref_val);
-    updateMonthlyData(ref_key, ref_val);
+    updateMonthlyUKData(ref_key, ref_val);
 }
 
-function updateMonthlyData(key, val) {
+function updateMonthlyUKData(key, val) {
     // console.log(key.length);
     // console.log(val.length);
     key.forEach(value => {
-        monthly_dates.push(value);
+        uk_monthly_dates.push(value);
     });
 
-    console.log(monthly_dates);
+    console.log(uk_monthly_dates);
 
     val.forEach(data => {
         var temp_arr = [];
@@ -50,30 +50,30 @@ function updateMonthlyData(key, val) {
         temp_arr.push(parseFloat(data['2. high']));
         temp_arr.push(parseFloat(data['3. low']));
         temp_arr.push(parseFloat(data['4. close']));
-        monthly_ohlc.push(temp_arr);
+        uk_monthly_ohlc.push(temp_arr);
     });
-    console.log(monthly_ohlc);
-    mergeMonthlyData();
+    console.log(uk_monthly_ohlc);
+    mergeUKMonthlyData();
 }
 
-function mergeMonthlyData(){
-    for (var i = 0; i < monthly_dates.length; i++) {
+function mergeUKMonthlyData(){
+    for (var i = 0; i < uk_monthly_dates.length; i++) {
         var resi = {};
         // console.log(objects [i]);
-        resi['x'] = monthly_dates[i];
+        resi['x'] = uk_monthly_dates[i];
         //console.log(i);
-        final_monthly.push(resi);
+        uk_final_monthly.push(resi);
     }
 
     for (var j = 0; j < monthly_ohlc.length; j++) {
-        final_monthly[j]['y'] = monthly_ohlc[j];
+        uk_final_monthly[j]['y'] = uk_monthly_ohlc[j];
     }
 
-    console.log(final_monthly);
+    console.log(uk_final_monthly);
     // loaderDouble.style.display = 'none';
     var options = {
         series: [{
-            data: final_monthly,
+            data: uk_final_monthly,
         }],
         chart: {
             type: 'candlestick',
@@ -104,7 +104,7 @@ function mergeMonthlyData(){
         }
     };
 
-    const div = document.getElementById('shareMonthlyChart');
+    const div = document.getElementById('UKMonthlyChart');
 
 
     var chart = new ApexCharts(div, options);
