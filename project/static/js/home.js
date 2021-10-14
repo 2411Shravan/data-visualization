2e302febaadd4ab3cd3756fabf2e1d27b76c127d
Shravan .V <perfectparameter24@gmail.com>
2021-10-14 13:18:47 +0530

    fixing ui of home page

--- project/static/js/home.js
+++ project/static/js/home.js
@@ -290,7 +290,7 @@ console.log(iconurls[0]);
   }],
     chart: {
     type: 'candlestick',
-    height: 200
+    height: 150
   },
   title: {
     text: ' ',
@@ -308,3 +308,40 @@ console.log(iconurls[0]);
 
   var chart = new ApexCharts(document.querySelector("#chart"), options);
   chart.render();
+
+
+  var optionsi = {
+    series: [{
+      name: " ",
+      data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+  }],
+    chart: {
+    height: 150,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: ' ',
+    align: 'left'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+  }
+  };
+
+  var charti = new ApexCharts(document.querySelector("#ichart"), optionsi);
+  charti.render();
